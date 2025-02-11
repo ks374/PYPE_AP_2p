432a95044da0166c5efaea36c3b5bed73971b59d
ks374 <chenghaz@terpmail.umd.edu>
2025-02-11 15:09:16 -0800

    Some utility update

---
+++ 64bit_PYPE_Feb2025_WorkingCopy/src/dacq-2p/openiris_server.h
@@ -0,0 +1,10 @@
+#ifndef OPENIRIS_SERVER_H
+#define OPENIRIS_SERVER_H
+
+#include "Openiris_client.h"
+#include "Eth32_interface.h"
+#include "dacqinfo.h"
+
+int init(); //test connection to eyetracker and eth32. Test dacq_data shared memory segment creation. 
+
+#endif
